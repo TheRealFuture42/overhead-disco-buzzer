3f9e6c0ff5e677b6cb2aa064d480eb1cf44e1913
TheRealFuture42 <trixxser@pm.me>
2023-09-08 19:28:31 +0200

    Som tests

--- player.py
+++ player.py
@@ -12,18 +12,27 @@ led = LED(13)
 # Pygame .init
 pygame.mixer.init()
 
+last_song = None
+
 def play_random_song():
+    global last_song
     print("Button was pressed")
 
     songs = [f for f in os.listdir(MUSIC_DIR) if f.endswith('.mp3')]
+
+    if last_song:
+        songs.remove(last_song)
+
     current_song = random.choice(songs)
+    last_song = current_song
     print(f"Song: {current_song}")
+
     pygame.mixer.music.load(os.path.join(MUSIC_DIR, current_song))
     time.sleep(0.1)
     pygame.mixer.music.play()
 
     led.on()
-    time.sleep(30)
+    time.sleep(5)
 
     led.blink(on_time=0.5, off_time=0.5)
 
